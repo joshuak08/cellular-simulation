5b5245b879c9f3d46dbf608c70ea0c67d36b4af7
rlarg <pharadonlarg@gmail.com>
2022-11-01 19:49:06 +0000

    filename from parameters and 2d slice of world

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,5 +1,7 @@
 package gol
 
+import "strconv"
+
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -12,7 +14,23 @@ type distributorChannels struct {
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 
+	// Create filename from parameters and send down the filename channel
+	filename := strconv.Itoa(p.ImageHeight) + "x" + strconv.Itoa(p.ImageHeight)
+	println(filename)
+	c.ioFilename <- filename
+
 	// TODO: Create a 2D slice to store the world.
+	world := make([][]byte, p.ImageHeight)
+	for i := range world {
+		world[i] = make([]byte, p.ImageWidth)
+	}
+
+	// Receive image byte by byte and store in 2d world
+	for i := 0; i < p.ImageHeight; i++ {
+		for j := 0; j < p.ImageWidth; j++ {
+			world[i][j] = <-c.ioInput
+		}
+	}
 
 	turn := 0
 
